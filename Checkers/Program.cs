808feff626bae67cdb1c15695a45850bef4ba8af
louise-poole <louisecarmenpoole@gmail.com>
2018-04-12 14:33:34 +0200

    Create initial interfaces

--- Checkers/Program.cs
+++ Checkers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Checkers
 {
@@ -8,5 +9,45 @@ namespace Checkers
         {
             Console.WriteLine("Hello World!");
         }
+        
     }
+
+    public interface IBoard
+    {
+        IPiece Occupant(int position);
+        IEnumerable<int> Pieces(Color color);
+        void Move(IPiece piece, int destination);
+        void RemoveCapturedPieces();
+        void Promote(IPiece piece);
+    }
+
+    public enum Color { Black, White }
+
+    public enum Status { Captured, Active }
+
+    public interface IPiece
+    {
+        IEnumerable<int> NormalMoves(IBoard board);
+        IEnumerable<int> CapturingMoves(IBoard board);
+        Status Status { get; }
+        Color Color { get; }
+        int Position { get; }
+        void Move(int destination);
+    }
+
+    public interface IPlayer
+    {
+        string Name { get; }
+        (int, int) GetMove();
+    }
+
+    public interface IReferee
+    {
+        IPlayer Winner();
+        bool IsDraw();
+        void Play();
+    }
+
+    public class IllegalMoveException : ApplicationException { }
+
 }
